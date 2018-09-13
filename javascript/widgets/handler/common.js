4fa2079353d60e70f52f24caa607e839023e9ccf
danny <danny@anovaculinary.com>
2018-09-13 10:49:31 -0700

    add TODOs for future cards re: error handling

--- javascript/widgets/handler/common.js
+++ javascript/widgets/handler/common.js
@@ -1476,6 +1476,7 @@ firebaseui.auth.widget.handler.common.handleSignInFetchSignInMethodsForEmail =
               } else if (xmlhttp.status === 404) {
                 resolve(false)
               } else {
+                // TODO need better error handling
                 reject({
                   status: xmlhttp.status,
                   statusText: "Error validating email against Cognito."
@@ -1484,6 +1485,7 @@ firebaseui.auth.widget.handler.common.handleSignInFetchSignInMethodsForEmail =
             }
 
             xmlhttp.onerror = function () {
+              // TODO need better error handling
               reject({
                 status: xmlhttp.status,
                 statusText: "Error validating email against Cognito."
@@ -1519,6 +1521,7 @@ firebaseui.auth.widget.handler.common.handleSignInFetchSignInMethodsForEmail =
 
       })
       .catch(function(error) {
+        // TODO need better error handling
         console.log(error.status + " " + error.statusText)
       })
 
